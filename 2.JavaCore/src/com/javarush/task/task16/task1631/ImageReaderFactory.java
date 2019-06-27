d9ab9c0e932fdf54c88f44b8279fba7ec805a3e3
TjavaistT <TjavaistT@gmail.com>
2019-06-27 17:18:17 +0300

    jrt 2:6(31)

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task16.task1631;
+
+import com.javarush.task.task16.task1631.common.*;
+
+public class ImageReaderFactory {
+
+    public static ImageReader getImageReader(ImageTypes imageType){
+        ImageReader reader = null;
+        switch (imageType){
+            case BMP:
+                 reader = new BmpReader();
+                break;
+            case JPG:
+                reader = new JpgReader();
+                break;
+            case PNG:
+                reader = new PngReader();
+                break;
+            default:
+                throw new IllegalArgumentException("Неизвестный тип картинки");
+        }
+
+        return reader;
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task16.task1631;
+
+import com.javarush.task.task16.task1631.common.ImageReader;
+import com.javarush.task.task16.task1631.common.ImageTypes;
+
+/* Возможно тут понаглядней чем при создании плееров и ботов
+*
+Задача 1631 - Фабрика
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        ImageReader reader = ImageReaderFactory.getImageReader(ImageTypes.JPG);
+    }
+}
