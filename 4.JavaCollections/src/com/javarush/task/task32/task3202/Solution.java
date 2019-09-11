1da85206b1e8e827b389b1f4d10c70a995920c42
TjavaistT <TjavaistT@gmail.com>
2019-09-11 12:59:10 +0300

    jrt 4:02(03)

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3202;
+
+import java.io.*;
+
+/* 
+Читаем из потока
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        if (is == null) return new StringWriter();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringWriter sw = new StringWriter();
+        String line;
+
+        while ((line = br.readLine()) != null){
+            sw.write(line);
+        }
+
+        return sw;
+    }
+}
